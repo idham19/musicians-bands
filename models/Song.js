845c7ab60768ab7b967f1a18ec2bdbcff25d50d7
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-18 15:43:03 -0400

    getLongestSong() method added to Song class

--- models/Song.js
+++ models/Song.js
@@ -1,30 +1,42 @@
-const {Sequelize, sequelize} = require('../db');
-const{Model,DataTypes}=require("sequelize")
+const { Sequelize, sequelize } = require("../db");
+const { Model, DataTypes } = require("sequelize");
 
 // TODO - define the Song model
-class Song extends Model{
-    toMinutes() {
-        const toMinute=(this.length / 60).toFixed(2); // Convert seconds to minutes
-        return `Music duration in minute is : ${toMinute} minutes`
-    }
-};
-Song.init({
-title:DataTypes.STRING,
-year:DataTypes.INTEGER,
-length:DataTypes.INTEGER
-},{
+class Song extends Model {
+  toMinutes() {
+    const toMinute = (this.length / 60).toFixed(2); // Convert seconds to minutes
+    return `Music duration in minute is : ${toMinute} minutes`;
+  }
+
+  static async getLongestSong() {
+    return await Song.findOne({
+      order: [["length", "DESC"]], // Find the song with the l
+    });
+  }
+}
+
+
+Song.init(
+  {
+    title: DataTypes.STRING,
+    year: DataTypes.INTEGER,
+    length: DataTypes.INTEGER,
+  },
+  {
     sequelize,
-    modelName:"Song"
-})
-const song1= new Song({
-    title:"azul",
-    year:2024,
-    length:190
-})
-const getSong1= Song.findByPk(1).then(data=>{
-    console.log(data.toMinutes());
-    
-})
+    modelName: "Song",
+  }
+);
+const song1 = new Song({
+  title: "azul",
+  year: 2024,
+  length: 190,
+});
+
+Song.getLongestSong().then((song) => {
+    console.log(song);
+  });
+
 module.exports = {
-    Song
+  Song,
 };
