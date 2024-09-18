d9edc88040733c8e2eec6a3751a6f6dd6dd27c48
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-18 15:13:40 -0400

    toMinutes() added to Song class

--- models/Song.js
+++ models/Song.js
@@ -2,7 +2,12 @@ const {Sequelize, sequelize} = require('../db');
 const{Model,DataTypes}=require("sequelize")
 
 // TODO - define the Song model
-class Song extends Model{};
+class Song extends Model{
+    toMinutes() {
+        const toMinute=(this.length / 60).toFixed(2); // Convert seconds to minutes
+        return `Music duration in minute is : ${toMinute} minutes`
+    }
+};
 Song.init({
 title:DataTypes.STRING,
 year:DataTypes.INTEGER,
@@ -11,7 +16,15 @@ length:DataTypes.INTEGER
     sequelize,
     modelName:"Song"
 })
-
+const song1= new Song({
+    title:"azul",
+    year:2024,
+    length:190
+})
+const getSong1= Song.findByPk(1).then(data=>{
+    console.log(data.toMinutes());
+    
+})
 module.exports = {
     Song
 };
