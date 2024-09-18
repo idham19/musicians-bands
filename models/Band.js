4c2866d03e2d108158286057f847190598d78989
MAHDI BELABBAS <mahdibelabbas@gmail.com>
2024-09-18 15:03:10 -0400

    toString() method added to Band Class

--- models/Band.js
+++ models/Band.js
@@ -1,22 +1,38 @@
-const {Sequelize, sequelize} = require('../db');
-const{Model,DataTypes} =require("sequelize")
+const { Sequelize, sequelize } = require("../db");
+const { Model, DataTypes } = require("sequelize");
 
 // TODO - define the Band model
-class Band extends Model{
-    toString() {
-        return `Band: ${this.name} Genre: ${this.genre}`;
-      }
-};
+class Band extends Model {
+  toString() {
+    return `Band: ${this.name} Genre: ${this.genre}`;
+  }
+}
 
-Band.init({
-name:DataTypes.STRING,
-genre:DataTypes.STRING,
-showCount:DataTypes.INTEGER
-},{
+Band.init(
+  {
+    name: DataTypes.STRING,
+    genre: DataTypes.STRING,
+    showCount: DataTypes.INTEGER,
+  },
+  {
     sequelize,
-    modelName:"Band"
-})
+    modelName: "Band",
+  }
+);
+const myBand = new Band({
+    name: "The Rolling Stones",
+    genre: "Rock",
+    showCount: 2000,
+  });
+  
+  const bandString = myBand.toString();
+  console.log(bandString); 
+const band1=Band.findByPk(2).then(data=>{
+    console.log(data.toString())
+});
+;
+
 
 module.exports = {
-    Band
+  Band,
 };
